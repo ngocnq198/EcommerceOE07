a615423d54f4e1da6129c3b0344e020be3551a13
nqngoc198 <nguyenquangngoc1981995@gmail.com>
2018-06-01 13:22:01 +0700

    facebook-like

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -34,6 +34,14 @@
     }
   });
 
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.0';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
   $(document).ready(function(){
     $('#home-slick').slick({
       autoplay: true,
